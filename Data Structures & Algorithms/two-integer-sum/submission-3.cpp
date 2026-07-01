72cc2a7b9833e1a9301a11ea58db6f950920ee2b
Nariman-Sayed <162900760+Nariman-Sayed@users.noreply.github.com>
2026-07-01 15:16:06 +0300

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> VTwoSum;
+        map <int,int> Contains_Two_Sum;
+        for(int i =0;i<nums.size();i++){
+             if(Contains_Two_Sum.find(target-nums[i])!=Contains_Two_Sum.end()){
+            VTwoSum.push_back(Contains_Two_Sum[target-nums[i]]);
+            VTwoSum.push_back(i);
+            return VTwoSum;
+             }
+             Contains_Two_Sum[nums[i]]=i;
+        }
+       return VTwoSum;
+    }
+};
