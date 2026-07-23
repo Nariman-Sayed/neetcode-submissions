3c5ec69578afdb7c8b139cd9b2af5823618d076d
Nariman-Sayed <162900760+Nariman-Sayed@users.noreply.github.com>
2026-07-23 07:46:34 +0300

    Add: arranging-coins - submission-3

---
+++ Data Structures & Algorithms/arranging-coins/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i =1;
+        int c = 0;
+        while(true){
+            n-= i;
+            if(n < 0){
+                break;
+            }
+            c++;
+            i++;
+        }
+        return c;
+    }
+};
