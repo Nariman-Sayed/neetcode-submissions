9f363bbed2e114c90239cd8ffa3756796b7bfe2a
Nariman-Sayed <162900760+Nariman-Sayed@users.noreply.github.com>
2026-08-30 17:03:41 +0300

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.cpp
@@ -0,0 +1,42 @@
+ class TimeMap {
+public:
+
+    unordered_map<string, vector<pair<int, string>>> mp;
+
+    TimeMap() {
+        
+    }
+
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+
+        if (mp.find(key) == mp.end())
+            return "";
+
+        int l = 0;
+        int r = mp[key].size() - 1;
+
+        int ans = -1;
+
+        while (l <= r) {
+
+            int mid = (l + r) / 2;
+
+            if (mp[key][mid].first <= timestamp) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        if (ans == -1)
+            return "";
+
+        return mp[key][ans].second;
+    }
+};
